40e9e4bbebc5e9fa581222573ed1745bd5709da9
Md Mahadi Hassan <sibathasan@gmail.com>
2025-03-18 17:22:40 -0400

    make the milu dataset for llama 3.2b

--- run_script.sh
+++ run_script.sh
@@ -2,7 +2,7 @@ python src/finetune/t5_loop.py --model_name t5-small --fold_count 2 --dataset me
 python src/finetune/t5_loop.py --model_name t5-small --fold_count 3 --dataset meta-llama/Llama-3.2-3B-evals --batch_size 32 --epochs 3 --learning_rate 5e-5 --save_path ./output/t5_small/fold_3
 python src/finetune/t5_loop.py --model_name t5-small --fold_count 4 --dataset meta-llama/Llama-3.2-3B-evals --batch_size 32 --epochs 3 --learning_rate 5e-5 --save_path ./output/t5_small/fold_4
 python src/finetune/t5_loop.py --model_name t5-small --fold_count 5 --dataset meta-llama/Llama-3.2-3B-evals --batch_size 32 --epochs 3 --learning_rate 5e-5 --save_path ./output/t5_small/fold_5
-python src/finetune/t5_loop.py --model_name t5-large --fold_count 2 --dataset meta-llama/Llama-3.2-3B-evals --batch_size 32 --epochs 3 --learning_rate 5e-5 --save_path ./output/t5_large/fold_2
-python src/finetune/t5_loop.py --model_name t5-large --fold_count 3 --dataset meta-llama/Llama-3.2-3B-evals --batch_size 32 --epochs 3 --learning_rate 5e-5 --save_path ./output/t5_large/fold_3
-python src/finetune/t5_loop.py --model_name t5-large --fold_count 4 --dataset meta-llama/Llama-3.2-3B-evals --batch_size 32 --epochs 3 --learning_rate 5e-5 --save_path ./output/t5_large/fold_4
-python src/finetune/t5_loop.py --model_name t5-large --fold_count 5 --dataset meta-llama/Llama-3.2-3B-evals --batch_size 32 --epochs 3 --learning_rate 5e-5 --save_path ./output/t5_large/fold_5
+python src/finetune/t5_loop.py --model_name t5-large --fold_count 2 --dataset meta-llama/Llama-3.2-3B-evals --batch_size 8 --epochs 3 --learning_rate 5e-5 --save_path ./output/t5_large/fold_2
+python src/finetune/t5_loop.py --model_name t5-large --fold_count 3 --dataset meta-llama/Llama-3.2-3B-evals --batch_size 8 --epochs 3 --learning_rate 5e-5 --save_path ./output/t5_large/fold_3
+python src/finetune/t5_loop.py --model_name t5-large --fold_count 4 --dataset meta-llama/Llama-3.2-3B-evals --batch_size 8 --epochs 3 --learning_rate 5e-5 --save_path ./output/t5_large/fold_4
+python src/finetune/t5_loop.py --model_name t5-large --fold_count 5 --dataset meta-llama/Llama-3.2-3B-evals --batch_size 8 --epochs 3 --learning_rate 5e-5 --save_path ./output/t5_large/fold_5
